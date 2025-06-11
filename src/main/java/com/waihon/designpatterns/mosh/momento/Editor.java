c271b0e0ba0f955922455c4f95dd94662f6b2f43
Waihon Yew <yewwaihon@gmail.com>
2025-06-12 01:38:07 +0800

    Add createState() to Editor class

--- src/main/java/com/waihon/designpatterns/mosh/momento/Editor.java
+++ src/main/java/com/waihon/designpatterns/mosh/momento/Editor.java
@@ -3,6 +3,10 @@ package com.waihon.designpatterns.mosh.momento;
 public class Editor {
     private String content;
 
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
     public String getContent() {
         return content;
     }
