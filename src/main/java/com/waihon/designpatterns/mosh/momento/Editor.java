be7b5a38c3715b76fbcb5830ee1a6d83bbb7a628
Waihon Yew <yewwaihon@gmail.com>
2025-06-11 23:59:58 +0800

    Create Editor class that can set and get content

---
+++ src/main/java/com/waihon/designpatterns/mosh/momento/Editor.java
@@ -0,0 +1,13 @@
+package com.waihon.designpatterns.mosh.momento;
+
+public class Editor {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
