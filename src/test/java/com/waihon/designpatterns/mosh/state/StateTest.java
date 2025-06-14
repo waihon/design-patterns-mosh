3c153e49fbbc27efd013509be12883419a8f847d
Waihon Yew <yewwaihon@gmail.com>
2025-06-14 08:37:55 +0800

    Test that canvas should show selection icon

---
+++ src/test/java/com/waihon/designpatterns/mosh/state/StateTest.java
@@ -0,0 +1,38 @@
+package com.waihon.designpatterns.mosh.state;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StateTest {
+
+    Canvas canvas;
+
+    @BeforeEach
+    void setUp() {
+        canvas = new Canvas();
+    }
+
+    @Nested
+    class GivenCurrentToolIsSelection {
+
+        @BeforeEach
+        void selectTool() {
+            canvas.setCurrentTool(ToolType.SELECTION);
+        }
+
+        @Nested
+        class WhenMouseDown {
+
+            @Test
+            void canvasShouldShowSelectionIcon() {
+                canvas.mouseDown();
+
+                assertThat(canvas.getCurrentIcon()).isEqualTo(IconType.SELECTION);
+            }
+        }
+
+    }
+}
