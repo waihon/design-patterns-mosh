95d395532e76b6fafef64ee600f10ea80077214e
Waihon Yew <yewwaihon@gmail.com>
2025-06-14 23:14:21 +0800

    Fully implement the Stopwatch

--- src/main/java/com/waihon/designpatterns/mosh/state/abuse/Stopwatch.java
+++ src/main/java/com/waihon/designpatterns/mosh/state/abuse/Stopwatch.java
@@ -5,9 +5,12 @@ public class Stopwatch {
     private boolean isRunning = false;
 
     public void click() {
-        if (!isRunning) {
+        if (isRunning) {
+            System.out.println("Stopped");
+        } else {
             System.out.println("Running");
         }
+        isRunning = !isRunning;
     }
 
 }
