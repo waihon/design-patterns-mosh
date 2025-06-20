b255cff518e9b3409978426b13da1980f8699fdd
Waihon Yew <yewwaihon@gmail.com>
2025-06-20 19:33:51 +0800

    Set up a Browser History object before each test

--- src/test/java/com/waihon/designpatterns/mosh/iterator/SimpleBrowserHistoryTest.java
+++ src/test/java/com/waihon/designpatterns/mosh/iterator/SimpleBrowserHistoryTest.java
@@ -1,21 +1,27 @@
 package com.waihon.designpatterns.mosh.iterator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SimpleBrowserHistoryTest {
 
+    SimpleBrowserHistory history;
+
+    @BeforeEach
+    void setUp() {
+        history = new SimpleBrowserHistory();
+    }
+
     @Test
     void historyShouldIncreaseInSizeWhenPushed() {
-        SimpleBrowserHistory history = new SimpleBrowserHistory();
         history.push("https://example.com");
         assertThat(history.getSize()).isEqualTo(1);
     }
 
     @Test
     void histroyShouldDecreaseInSizeWhenPopped() {
-        SimpleBrowserHistory history = new SimpleBrowserHistory();
         history.push("https://example.com");
         history.pop();
         assertThat(history.getSize()).isEqualTo(0);
@@ -23,7 +29,6 @@ public class SimpleBrowserHistoryTest {
 
     @Test
     void historyShouldReturnLastItemWhenPopped() {
-        SimpleBrowserHistory history = new SimpleBrowserHistory();
         history.push("https://example.com");
         history.push("https://example.org");
         String lastItem = history.pop();
@@ -32,7 +37,6 @@ public class SimpleBrowserHistoryTest {
 
     @Test
     void emptyHistoryShouldReturnNullWhenPopped() {
-        SimpleBrowserHistory history = new SimpleBrowserHistory();
         String lastItem = history.pop();
         assertThat(lastItem).isNull();
     }
