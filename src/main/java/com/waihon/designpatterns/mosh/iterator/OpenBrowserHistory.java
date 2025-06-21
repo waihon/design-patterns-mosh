c8b915c5c05e3d63c0f42a8a312ba2fc1efa748e
Waihon Yew <yewwaihon@gmail.com>
2025-06-21 15:07:20 +0800

    OpenBrowserHistory can create Iterator

--- src/main/java/com/waihon/designpatterns/mosh/iterator/OpenBrowserHistory.java
+++ src/main/java/com/waihon/designpatterns/mosh/iterator/OpenBrowserHistory.java
@@ -23,4 +23,33 @@ public class OpenBrowserHistory {
         return urls.removeLast(); // Java 21
     }
 
+    public Iterator<String> createIterator() {
+        return new ListIterator(this);
+    }
+
+    public class ListIterator implements Iterator<String> {
+
+        private OpenBrowserHistory history;
+        private int index;
+
+        public ListIterator(OpenBrowserHistory history) {
+            this.history = history;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < history.getSize());
+        }
+
+        @Override
+        public String current() {
+            return history.urls.get(index);
+        }
+
+        @Override
+        public void next() {
+            index++;
+        }
+    }
+
 }
