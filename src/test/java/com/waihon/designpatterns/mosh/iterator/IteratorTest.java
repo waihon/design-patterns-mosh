1589292b4102778f15458797cf4d3cdd5ff4b3d0
Waihon Yew <yewwaihon@gmail.com>
2025-06-21 14:57:44 +0800

    Iterator can iterate over BrowserHistory items

---
+++ src/test/java/com/waihon/designpatterns/mosh/iterator/IteratorTest.java
@@ -0,0 +1,36 @@
+package com.waihon.designpatterns.mosh.iterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class IteratorTest {
+    OpenBrowserHistory history;
+    Iterator<String> iterator;
+
+    @BeforeEach
+    void setUp() {
+        history = new OpenBrowserHistory();
+        history.push("https://example.com");
+        history.push("https://example.org");
+        iterator = history.createIterator();
+    }
+
+    @Test
+    void iteratorCanIterateOverBrowserHistory() {
+        List<String> urls = new ArrayList<>();
+        while (iterator.hasNext()) {
+            String current = iterator.current();
+            urls.add(current);
+            iterator.next();
+        }
+
+        assertThat(urls).isEqualTo(Arrays.asList("https://example.com", "https://example.org"));
+    }
+
+}
