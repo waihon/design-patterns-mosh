29ae82b5eac0889edd50d8c624ea362b3c91ab60
Waihon Yew <yewwaihon@gmail.com>
2025-06-22 11:43:56 +0800

    Test that ImageStorage should compress using JPEG

---
+++ src/test/java/com/waihon/designpatterns/mosh/strategy/ImageStorageTest.java
@@ -0,0 +1,49 @@
+package com.waihon.designpatterns.mosh.strategy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ImageStorageTest {
+
+    private ImageStorage imageStorage;
+    private String fileName;
+
+    @BeforeEach
+    void setUp() {
+        imageStorage = new ImageStorage();
+    }
+
+    @Nested
+    class WhenStoringAnImage {
+
+        @Nested
+        class WithCompressor {
+
+            @Nested
+            class SetToJpeg {
+
+                @BeforeEach
+                void selectCompressor() {
+                    imageStorage.setCompressor(CompressorType.JPEG);
+                }
+
+                @Test
+                void imageStorageShouldCompressUsingJpeg() throws Exception {
+                    String text = tapSystemOut(() -> {
+                        imageStorage.store(fileName);
+                    });
+
+                    assertThat(text).contains("Compressing using JPEG.");
+
+                }
+            }
+
+        }
+
+    }
+
+}
