37e386630b276feb12e746ba98ac818f6e52bbec
Waihon Yew <yewwaihon@gmail.com>
2025-09-14 23:37:15 +0800

    Demo using the new User class

---
+++ src/main/java/com/waihon/designpatterns/mosh/essentials/Main.java
@@ -0,0 +1,8 @@
+package com.waihon.designpatterns.mosh.essentials;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Waihon");
+        user.sayHello();
+    }
+}
