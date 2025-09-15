6000a7bbe916162efeae54223f8bb575532f1ff5
Waihon Yew <yewwaihon@gmail.com>
2025-09-15 18:27:02 +0800

    Create 2 implementations of TaxCalculator interface

---
+++ src/main/java/com/waihon/designpatterns/mosh/essentials/TaxCalculator2019.java
@@ -0,0 +1,12 @@
+package com.waihon.designpatterns.mosh.essentials;
+
+public class TaxCalculator2019 implements  TaxCalculator {
+    @Override
+    public float calculateTax() {
+        return 2019;
+    }
+
+    public float calculatePremium() {
+        return 2000;
+    }
+}
---
+++ src/main/java/com/waihon/designpatterns/mosh/essentials/TaxCalculator2020.java
@@ -0,0 +1,9 @@
+package com.waihon.designpatterns.mosh.essentials;
+
+public class TaxCalculator2020 implements TaxCalculator {
+
+    @Override
+    public float calculateTax() {
+        return 2020;
+    }
+}
