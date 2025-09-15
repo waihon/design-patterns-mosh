d41dc6b4319529b450a9a865ffb514e2bab0a65d
Waihon Yew <yewwaihon@gmail.com>
2025-09-15 18:29:16 +0800

    Calculate tax using implementations of TaxCalculator

--- src/main/java/com/waihon/designpatterns/mosh/essentials/Main.java
+++ src/main/java/com/waihon/designpatterns/mosh/essentials/Main.java
@@ -1,8 +1,23 @@
 package com.waihon.designpatterns.mosh.essentials;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         User user = new User("Waihon");
         user.sayHello();
+
+        TaxCalculator calculator = getCalculator();
+        System.out.println(calculator.calculateTax());
+    }
+
+    public static TaxCalculator getCalculator() {
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            return new TaxCalculator2019();
+        } else {
+            return new TaxCalculator2020();
+        }
+
     }
 }
