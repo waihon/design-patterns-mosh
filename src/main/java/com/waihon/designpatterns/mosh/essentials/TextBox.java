8a8620e33da922e76c9327bba82afa848239380f
Waihon Yew <yewwaihon@gmail.com>
2025-09-17 22:26:30 +0800

    Implement draw() for TextBox

--- src/main/java/com/waihon/designpatterns/mosh/essentials/TextBox.java
+++ src/main/java/com/waihon/designpatterns/mosh/essentials/TextBox.java
@@ -1,4 +1,8 @@
 package com.waihon.designpatterns.mosh.essentials;
 
 public class TextBox extends UIControl {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a text box");
+    }
 }
