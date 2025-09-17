841fb82ed3ce5d57413fdcb83d48fe072f217024
Waihon Yew <yewwaihon@gmail.com>
2025-09-17 23:18:52 +0800

    Test the behaviours of GenerateReportTask

---
+++ src/test/java/com/waihon/designpatterns/mosh/template/GenerateReportTaskTest.java
@@ -0,0 +1,34 @@
+package com.waihon.designpatterns.mosh.template;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GenerateReportTaskTest {
+    private GenerateReportTask task;
+
+    @BeforeEach
+    void setUp() {
+        task = new GenerateReportTask();
+    }
+
+    @Test
+    void shouldAudit() throws Exception {
+        String text = tapSystemOut(() -> {
+            task.execute();
+        });
+
+        assertThat(text).contains("Audit");
+    }
+
+    @Test
+    void shouldGenerateReport() throws Exception {
+        String text = tapSystemOut(() -> {
+            task.execute();
+        });
+
+        assertThat(text).contains("Generate Report");
+    }
+}
