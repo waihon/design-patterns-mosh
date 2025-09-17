c7e0032e325d58ff09332c6f7a8336d509ac58fc
Waihon Yew <yewwaihon@gmail.com>
2025-09-17 23:18:27 +0800

    Basic implementation of TransferMoneyTask

---
+++ src/main/java/com/waihon/designpatterns/mosh/template/TransferMoneyTask.java
@@ -0,0 +1,8 @@
+package com.waihon.designpatterns.mosh.template;
+
+public class TransferMoneyTask {
+    public void execute() {
+        System.out.println("Audit");
+        System.out.println("Transfer Money");
+    }
+}
