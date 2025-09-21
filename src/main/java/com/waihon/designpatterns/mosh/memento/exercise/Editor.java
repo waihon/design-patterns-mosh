f321ea15a8e4b69eb57e7526f4cba102bb444512
Waihon Yew <yewwaihon@gmail.com>
2025-09-21 12:40:41 +0800

    Create an Editor class has a Document

---
+++ src/main/java/com/waihon/designpatterns/mosh/memento/exercise/Editor.java
@@ -0,0 +1,13 @@
+package com.waihon.designpatterns.mosh.memento.exercise;
+
+public class Editor {
+    private Document document;
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+}
