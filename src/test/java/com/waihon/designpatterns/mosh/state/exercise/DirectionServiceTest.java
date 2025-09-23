34a5bc7c72cc66d96f717b941a6f5233ec02d3d5
Waihon Yew <yewwaihon@gmail.com>
2025-09-23 20:05:41 +0800

    Test first the behaviours of DirectionService

---
+++ src/test/java/com/waihon/designpatterns/mosh/state/exercise/DirectionServiceTest.java
@@ -0,0 +1,167 @@
+package com.waihon.designpatterns.mosh.state.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DirectionServiceTest {
+    private DirectionService directionService;
+
+    @BeforeEach
+    void setUp() {
+        directionService = new DirectionService();
+    }
+
+    @Nested
+    class GivenTravelModeIsDriving {
+        @BeforeEach
+        void selectTravelMode() {
+            directionService.setTravelMode(TravelMode.DRIVING);
+        }
+
+        @Nested
+        class whenGetEta {
+            @Test
+            void shouldReturnEta() throws Exception {
+                var eta = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    eta.set(directionService.getEta());
+                });
+
+                assertThat(text).contains("Calculating ETA (driving)");
+                assertThat(eta.get()).isEqualTo(1);
+
+            }
+        }
+
+        @Nested
+        class whenGetDirection {
+            @Test
+            void shouldReturnDirection() throws Exception  {
+                var direction = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    direction.set(directionService.getDirection());
+                });
+
+                assertThat(text).contains("Calculating Direction (driving)");
+                assertThat(direction.get()).isEqualTo(1);
+            }
+        }
+    }
+
+    @Nested
+    class GivenTravelModeIsBicycling {
+        @BeforeEach
+        void selectTravelMode() {
+            directionService.setTravelMode(TravelMode.BICYCLING);
+        }
+
+        @Nested
+        class whenGetEta {
+            @Test
+            void shouldReturnEta() throws Exception {
+                var eta = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    eta.set(directionService.getEta());
+                });
+
+                assertThat(text).contains("Calculating ETA (bicycling)");
+                assertThat(eta.get()).isEqualTo(2);
+
+            }
+        }
+
+        @Nested
+        class whenGetDirection {
+            @Test
+            void shouldReturnDirection() throws Exception  {
+                var direction = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    direction.set(directionService.getDirection());
+                });
+
+                assertThat(text).contains("Calculating Direction (bicycling)");
+                assertThat(direction.get()).isEqualTo(2);
+            }
+        }
+    }
+
+    @Nested
+    class GivenTravelModeIsTransit {
+        @BeforeEach
+        void selectTravelMode() {
+            directionService.setTravelMode(TravelMode.TRANSIT);
+        }
+
+        @Nested
+        class whenGetEta {
+            @Test
+            void shouldReturnEta() throws Exception {
+                var eta = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    eta.set(directionService.getEta());
+                });
+
+                assertThat(text).contains("Calculating ETA (transit)");
+                assertThat(eta.get()).isEqualTo(3);
+
+            }
+        }
+
+        @Nested
+        class whenGetDirection {
+            @Test
+            void shouldReturnDirection() throws Exception  {
+                var direction = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    direction.set(directionService.getDirection());
+                });
+
+                assertThat(text).contains("Calculating Direction (transit)");
+                assertThat(direction.get()).isEqualTo(3);
+            }
+        }
+    }
+
+    @Nested
+    class GivenTravelModeIsWalking {
+        @BeforeEach
+        void selectTravelMode() {
+            directionService.setTravelMode(TravelMode.WALKING);
+        }
+
+        @Nested
+        class whenGetEta {
+            @Test
+            void shouldReturnEta() throws Exception {
+                var eta = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    eta.set(directionService.getEta());
+                });
+
+                assertThat(text).contains("Calculating ETA (walking)");
+                assertThat(eta.get()).isEqualTo(4);
+
+            }
+        }
+
+        @Nested
+        class whenGetDirection {
+            @Test
+            void shouldReturnDirection() throws Exception  {
+                var direction = new AtomicReference<Object>();
+                var text = tapSystemOut(() -> {
+                    direction.set(directionService.getDirection());
+                });
+
+                assertThat(text).contains("Calculating Direction (walking)");
+                assertThat(direction.get()).isEqualTo(4);
+            }
+        }
+    }
+}
