52b9ead86882af390ef617a8c8450861f087fbc8
Waihon Yew <yewwaihon@gmail.com>
2025-09-24 23:37:29 +0800

    Replace nested ifs with delegation to classes which implement Travel interface

--- src/main/java/com/waihon/designpatterns/mosh/state/exercise/DirectionService.java
+++ src/main/java/com/waihon/designpatterns/mosh/state/exercise/DirectionService.java
@@ -1,51 +1,21 @@
 package com.waihon.designpatterns.mosh.state.exercise;
 
 public class DirectionService {
-    private TravelMode travelMode;
+    private Travel travel;
 
     public Object getEta() {
-        if (travelMode == TravelMode.DRIVING) {
-            System.out.println("Calculating ETA (driving)");
-            return 1;
-        }
-        else if (travelMode == TravelMode.BICYCLING) {
-            System.out.println("Calculating ETA (bicycling)");
-            return 2;
-        }
-        else if (travelMode == TravelMode.TRANSIT) {
-            System.out.println("Calculating ETA (transit)");
-            return 3;
-        }
-        else {
-            System.out.println("Calculating ETA (walking)");
-            return 4;
-        }
+        return travel.getEta();
     }
 
     public Object getDirection() {
-        if (travelMode == TravelMode.DRIVING) {
-            System.out.println("Calculating Direction (driving)");
-            return 1;
-        }
-        else if (travelMode == TravelMode.BICYCLING) {
-            System.out.println("Calculating Direction (bicycling)");
-            return 2;
-        }
-        else if (travelMode == TravelMode.TRANSIT) {
-            System.out.println("Calculating Direction (transit)");
-            return 3;
-        }
-        else {
-            System.out.println("Calculating Direction (walking)");
-            return 4;
-        }
+        return travel.getDirection();
     }
 
-    public TravelMode getTravelMode() {
-        return travelMode;
+    public Travel getTravel() {
+        return travel;
     }
 
-    public void setTravelMode(TravelMode travelMode) {
-        this.travelMode = travelMode;
+    public void setTravel(Travel travel) {
+        this.travel = travel;
     }
 }
