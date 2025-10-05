d337a98a6db49659d6c30d15a11795dd6edeaf26
Waihon Yew <yewwaihon@gmail.com>
2025-10-05 22:15:21 +0800

    Test first the behaviours of BoldCommand

---
+++ src/test/java/com/waihon/designpatterns/mosh/command/editor/BoldCommandTest.java
@@ -0,0 +1,36 @@
+package com.waihon.designpatterns.mosh.command.editor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BoldCommandTest {
+    private BoldCommand boldCommand;
+    private String content;
+    private HtmlDocument document;
+
+    @BeforeEach
+    void setUp() {
+        document = new HtmlDocument();
+        content = "Hello World!";
+        document.setContent(content);
+        var history = new History();
+        boldCommand = new BoldCommand(document, history);
+    }
+
+    @Test
+    void canExecute() {
+        boldCommand.execute();
+
+        assertThat(document.getContent()).isEqualTo("<b>" + content + "</b>");
+    }
+
+    @Test
+    void canUnexecute() {
+        boldCommand.execute();
+        boldCommand.unexecute();
+
+        assertThat(document.getContent()).isEqualTo(content);
+    }
+}
