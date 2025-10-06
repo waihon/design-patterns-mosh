b27414369180feac0559c61ebbf4a98fc0ec7751
Waihon Yew <yewwaihon@gmail.com>
2025-10-06 22:18:24 +0800

    Create a History class for storing UndoableCommand objects

---
+++ src/main/java/com/waihon/designpatterns/mosh/command/editor/History.java
@@ -0,0 +1,21 @@
+package com.waihon.designpatterns.mosh.command.editor;
+
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class History {
+    private Deque<UndoableCommand> commands = new ArrayDeque<>();
+
+    public void push(UndoableCommand command) {
+        commands.push(command);
+    }
+
+    public UndoableCommand pop() {
+        return commands.pop();
+    }
+
+    public int size() {
+        return commands.size();
+    }
+}
