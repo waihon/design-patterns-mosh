f718d8b0b8d3231e61c4d87dc40ab97f24a0863e
Waihon Yew <yewwaihon@gmail.com>
2025-10-08 22:23:30 +0800

    Test first the behaviours of UndoCommand

---
+++ src/test/java/com/waihon/designpatterns/mosh/command/editor/UndoCommandTest.java
@@ -0,0 +1,33 @@
+package com.waihon.designpatterns.mosh.command.editor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UndoCommandTest {
+    private BoldCommand boldCommand;
+    private String content;
+    private HtmlDocument document;
+    private UndoCommand undoCommand;
+
+    @BeforeEach
+    void setUp() {
+        document = new HtmlDocument();
+        content = "Hello World!";
+        document.setContent(content);
+        var history = new History();
+
+        boldCommand = new BoldCommand(document, history);
+        boldCommand.execute();
+
+        undoCommand = new UndoCommand(history);
+    }
+
+    @Test
+    void canUnexecute() {
+        undoCommand.execute();
+
+        assertThat(document.getContent()).isEqualTo(content);
+    }
+}
