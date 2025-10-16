891898f1a2b285b0e485ea54b2a6ffa9b0e4c06c
Waihon Yew <yewwaihon@gmail.com>
2025-10-17 07:43:48 +0800

    Implement ContrastCommand

---
+++ src/main/java/com/waihon/designpatterns/mosh/command/exercise/ContrastCommand.java
@@ -0,0 +1,24 @@
+package com.waihon.designpatterns.mosh.command.exercise;
+
+public class ContrastCommand implements UndoableCommand {
+    private float prevContrast;
+    private float newContrast;
+    private VideoEditor videoEditor;
+    private History history;
+
+    public ContrastCommand(float newContrast, VideoEditor videoEditor, History history) {
+        this.newContrast = newContrast;
+        this.videoEditor = videoEditor;
+        this.history = history;
+    }
+
+    public void execute() {
+        prevContrast = videoEditor.getContrast();
+        videoEditor.setContrast(newContrast);
+        history.push(this);
+    }
+
+    public void unexecute() {
+        videoEditor.setContrast(prevContrast);
+    }
+}
