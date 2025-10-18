2af3f635a8e2c8e44f816d2f5ecb85fd4a1cefa7
Waihon Yew <yewwaihon@gmail.com>
2025-10-18 23:33:46 +0800

    Test first the behaviours of StatusBar

---
+++ src/test/java/com/waihon/designpatterns/mosh/observer/exercise/StatusBarTest.java
@@ -0,0 +1,36 @@
+package com.waihon.designpatterns.mosh.observer.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StatusBarTest {
+    private Stock stock1;
+    private Stock stock2;
+    private StatusBar statusBar;
+
+    @BeforeEach
+    void setUp() {
+        stock1 = new Stock("NVDA", 183.22f);
+        stock2 = new Stock("AAPL", 252.29f);
+
+        statusBar = new StatusBar();
+        statusBar.addStock(stock1);
+        statusBar.addStock(stock2);
+    }
+
+    @Test
+    void showStocks() throws Exception {
+        var text = tapSystemOut(() -> {
+            statusBar.show();
+        });
+
+        assertThat(text).contains("Stock{symbol='NVDA', price=183.22}");
+        assertThat(text).contains("Stock{symbol='AAPL', price=252.29}");
+        assertThat(text).doesNotContain("Stock{symbol='GOOG', price=253.79}");
+        assertThat(text).doesNotContain("Stock{symbol='AMZN', price=213.04}");
+        assertThat(text).doesNotContain("Stock{symbol='MSFT', price=513.58}");
+    }
+}
