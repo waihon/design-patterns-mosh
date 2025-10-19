161839fbeb702dc4201509e2d6214461a1fa8f92
Waihon Yew <yewwaihon@gmail.com>
2025-10-19 14:44:01 +0800

    Enhance StockListView to communicate update in stock prices

--- src/main/java/com/waihon/designpatterns/mosh/observer/exercise/StockListView.java
+++ src/main/java/com/waihon/designpatterns/mosh/observer/exercise/StockListView.java
@@ -3,7 +3,7 @@ package com.waihon.designpatterns.mosh.observer.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StockListView {
+public class StockListView implements Observer {
     private List<Stock> stocks = new ArrayList<>();
 
     public void addStock(Stock stock) {
@@ -13,5 +13,10 @@ public class StockListView {
     public void show() {
         for (var stock : stocks)
             System.out.println(stock);
+    }
+
+    @Override
+    public void update() {
+       show();
     }
 }
