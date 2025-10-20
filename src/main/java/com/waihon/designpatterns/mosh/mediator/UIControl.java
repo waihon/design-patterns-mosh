0040cf8559fe7d08c5b05e0d88bd67d788d72dd7
Waihon Yew <yewwaihon@gmail.com>
2025-10-20 09:52:45 +0800

    Create UIControl superclass

---
+++ src/main/java/com/waihon/designpatterns/mosh/mediator/UIControl.java
@@ -0,0 +1,9 @@
+package com.waihon.designpatterns.mosh.mediator;
+
+public class UIControl {
+    protected DialogBox owner;
+
+    public UIControl(DialogBox owner) {
+        this.owner = owner;
+    }
+}
