4dc9d47bace3cd2453e0a77d9ca335d1c2b055b2
Waihon Yew <yewwaihon@gmail.com>
2025-10-21 23:27:06 +0800

    Implement 2 concrete classes of HtmlNode interface

---
+++ src/main/java/com/waihon/designpatterns/mosh/visitor/AnchorNode.java
@@ -0,0 +1,8 @@
+package com.waihon.designpatterns.mosh.visitor;
+
+public class AnchorNode implements HtmlNode {
+    @Override
+    public void execute(Operation operation) {
+        operation.apply(this);
+    }
+}
