e66355afadd8d338bbec1887722c5510489d6859
Waihon Yew <yewwaihon@gmail.com>
2025-10-25 17:02:10 +0800

    Use default implementation of closeWindow() and improve output messages

--- src/main/java/com/waihon/designpatterns/mosh/template/exercise/DialogBox.java
+++ src/main/java/com/waihon/designpatterns/mosh/template/exercise/DialogBox.java
@@ -4,16 +4,11 @@ public class DialogBox extends Window {
 
     @Override
     protected void beforeClose() {
-        System.out.println("Dialog Box Before Close");
-    }
-
-    @Override
-    protected void closeWindow() {
-        System.out.println("Dialog Box Close Window");
+        System.out.println("Before closing Dialog Box");
     }
 
     @Override
     protected void afterClose() {
-        System.out.println("Dialog Box After Close");
+        System.out.println("After closing Dialog Box");
     }
 }
