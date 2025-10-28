a36d5bde137d71a77eeeb4fc346c991dc8bb4aea
Waihon Yew <yewwaihon@gmail.com>
2025-10-29 07:53:26 +0800

    Modify UndoCommandTest to refer to UndoCommand in fx package

--- src/test/java/com/waihon/designpatterns/mosh/command/editor/UndoCommandTest.java
+++ src/test/java/com/waihon/designpatterns/mosh/command/editor/UndoCommandTest.java
@@ -1,6 +1,7 @@
 package com.waihon.designpatterns.mosh.command.editor;
 
 import com.waihon.designpatterns.mosh.command.fx.History;
+import com.waihon.designpatterns.mosh.command.fx.UndoCommand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
