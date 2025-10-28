aa541a0154238d78f235a991b657e3afd6839d3c
Waihon Yew <yewwaihon@gmail.com>
2025-10-29 01:21:14 +0800

    Delete duplcate classes and interfaces from exercise package

--- src/main/java/com/waihon/designpatterns/mosh/command/exercise/Command.java
+++
@@ -1,5 +0,0 @@
-package com.waihon.designpatterns.mosh.command.exercise;
-
-public interface Command {
-    void execute();
-}
--- src/main/java/com/waihon/designpatterns/mosh/command/exercise/UndoableCommand.java
+++
@@ -1,7 +0,0 @@
-package com.waihon.designpatterns.mosh.command.exercise;
-
-import com.waihon.designpatterns.mosh.command.editor.Command;
-
-public interface UndoableCommand extends Command {
-    void unexecute();
-}
