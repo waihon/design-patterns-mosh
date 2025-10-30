7cc3b85334dbd4e70b1a138a8cc80b76e717b65e
Waihon Yew <yewwaihon@gmail.com>
2025-10-31 02:40:48 +0800

    Modify DataSource class to inherit from Subject

--- src/main/java/com/waihon/designpatterns/mosh/observer/pull/DataSource.java
+++ src/main/java/com/waihon/designpatterns/mosh/observer/pull/DataSource.java
@@ -3,23 +3,8 @@ package com.waihon.designpatterns.mosh.observer.pull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataSource {
-    private List<Observer> observers = new ArrayList<>();
+public class DataSource extends Subject {
     private int value;
-
-    public void addObserver(Observer observer) {
-        observers.add(observer);
-    }
-
-    public void removeObserver(Observer observer) {
-        observers.remove(observer);
-    }
-
-    public void notifyObservers() {
-        for (var observer : observers) {
-            observer.update();
-        }
-    }
 
     public int getValue() {
         return value;
