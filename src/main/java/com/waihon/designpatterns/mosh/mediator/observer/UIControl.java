a3b42b4f5c7544f16fb3b5b3babdf958b3c013ec
Waihon Yew <yewwaihon@gmail.com>
2025-11-01 15:36:06 +0800

    Update UIControl removing methods moved to the parent Subject

--- src/main/java/com/waihon/designpatterns/mosh/mediator/observer/UIControl.java
+++ src/main/java/com/waihon/designpatterns/mosh/mediator/observer/UIControl.java
@@ -3,20 +3,5 @@ package com.waihon.designpatterns.mosh.mediator.observer;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class UIControl {
-    private List<Observer> observers = new ArrayList<>();
-
-    public void attach(Observer observer) {
-        observers.add(observer);
-    }
-
-    public void unattach(Observer observer) {
-        observers.remove(observer);
-    }
-
-    protected void notifyObservers() {
-        for (var observer : observers) {
-            observer.update();
-        }
-    }
+public abstract class UIControl extends Subject {
 }
