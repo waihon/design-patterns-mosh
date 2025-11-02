cdbba677d7f88d1ff11452d0e89bf2aef3effff1
Waihon Yew <yewwaihon@gmail.com>
2025-11-02 21:53:49 +0800

    Create Shape, a concrete implementation of Component

---
+++ src/main/java/com/waihon/designpatterns/mosh/composite/Shape.java
@@ -0,0 +1,9 @@
+package com.waihon.designpatterns.mosh.composite;
+
+public class Shape implements Component {
+
+    @Override
+    public void render() {
+        System.out.println("Render Shape");
+    }
+}
