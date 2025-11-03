e51d001e571f80efc6ed67f645bb0f93f09f0fe4
Waihon Yew <yewwaihon@gmail.com>
2025-11-03 22:58:15 +0800

    Create Truck resource

---
+++ src/main/java/com/waihon/designpatterns/mosh/composite/exercise/Truck.java
@@ -0,0 +1,8 @@
+package com.waihon.designpatterns.mosh.composite.exercise;
+
+public class Truck implements Resource {
+    @Override
+    public void deploy() {
+        System.out.println("Deploying a truck");
+    }
+}
