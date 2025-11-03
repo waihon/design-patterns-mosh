8a083bf6a3d1fa78e3cb9f34fd91ce263b923801
Waihon Yew <yewwaihon@gmail.com>
2025-11-04 01:01:44 +0800

    Create Image class with a name

---
+++ src/main/java/com/waihon/designpatterns/mosh/adapter/demo/Image.java
@@ -0,0 +1,13 @@
+package com.waihon.designpatterns.mosh.adapter.demo;
+
+public class Image {
+    private String name;
+
+    public Image(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
