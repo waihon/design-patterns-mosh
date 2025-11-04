4ae49143e52122be36ec5b2de4d3889160f1bfa2
Waihon Yew <yewwaihon@gmail.com>
2025-11-05 00:33:20 +0800

    Add the 3rd party GmailClient

---
+++ src/main/java/com/waihon/designpatterns/mosh/adapter/exercise/gmail/GmailClient.java
@@ -0,0 +1,15 @@
+package com.waihon.designpatterns.mosh.adapter.exercise.gmail;
+
+public class GmailClient {
+    public void connect() {
+        System.out.println("Connecting to Gmail");
+    }
+
+    public void getEmails() {
+        System.out.println("Downloading emails from Gmail");
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnecting from Gmail");
+    }
+}
