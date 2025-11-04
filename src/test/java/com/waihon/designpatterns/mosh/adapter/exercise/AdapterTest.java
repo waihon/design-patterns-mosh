4ec2c9f58a6d0db4bdbda87278bd838bfc13300c
Waihon Yew <yewwaihon@gmail.com>
2025-11-05 00:32:38 +0800

    Test first the behaviours of Adapter pattern for email service

---
+++ src/test/java/com/waihon/designpatterns/mosh/adapter/exercise/AdapterTest.java
@@ -0,0 +1,67 @@
+package com.waihon.designpatterns.mosh.adapter.exercise;
+
+import com.waihon.designpatterns.mosh.adapter.exercise.gmail.GmailClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AdapterTest {
+    private static final String YAHOO_DOWNLOAD = "Downloading emails from Yahoo";
+    private static final String GMAIL_CONNECT = "Connecting to Gmail";
+    private static final String GMAIL_DOWNLOAD = "Downloading emails from Gmail";
+    private static final String GMAIL_DISCONNECT = "Disconnecting from Gmail";
+
+    private GmailClient gmailClient;
+    private EmailClient emailClient;
+    private YahooProvider yahooProvider;
+    private GmailProvider gmailProvider;
+
+    @BeforeEach
+    void setUp() {
+        yahooProvider = new YahooProvider();
+        gmailClient = new GmailClient();
+        gmailProvider = new GmailProvider(gmailClient);
+        
+        emailClient = new EmailClient();
+        emailClient.addProvider(yahooProvider);
+        emailClient.addProvider(gmailProvider);
+    }
+
+    @Test
+    void yahooProviderDownloadsEmails() throws Exception {
+        var text = tapSystemOut(() -> {
+            yahooProvider.downloadEmails();
+        });
+
+        assertThat(text).contains(YAHOO_DOWNLOAD);
+        assertThat(text).doesNotContain(GMAIL_CONNECT);
+        assertThat(text).doesNotContain(GMAIL_DOWNLOAD);
+        assertThat(text).doesNotContain(GMAIL_DISCONNECT);
+    }
+
+    @Test
+    void gmailProviderDownloadsEmails() throws Exception {
+        var text = tapSystemOut(() -> {
+            gmailProvider.downloadEmails();
+        });
+
+        assertThat(text).contains(GMAIL_CONNECT);
+        assertThat(text).contains(GMAIL_DOWNLOAD);
+        assertThat(text).contains(GMAIL_DISCONNECT);
+        assertThat(text).doesNotContain(YAHOO_DOWNLOAD);
+    }
+
+    @Test
+    void emailClientDownloadsEmails() throws Exception {
+        var text = tapSystemOut(() -> {
+            emailClient.downloadEmails();
+        });
+
+        assertThat(text).contains(YAHOO_DOWNLOAD);
+        assertThat(text).contains(GMAIL_CONNECT);
+        assertThat(text).contains(GMAIL_DOWNLOAD);
+        assertThat(text).contains(GMAIL_DISCONNECT);
+    }
+}
