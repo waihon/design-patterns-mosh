53611b3e37437ab223431289296e72415c609b30
Waihon Yew <yewwaihon@gmail.com>
2025-11-05 00:37:52 +0800

    Create EmailClient which talks to EmailProvider interface

---
+++ src/main/java/com/waihon/designpatterns/mosh/adapter/exercise/EmailClient.java
@@ -0,0 +1,18 @@
+package com.waihon.designpatterns.mosh.adapter.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailClient {
+    private List<EmailProvider> providers = new ArrayList<>();
+
+    public void addProvider(EmailProvider provider) {
+        providers.add(provider);
+    }
+
+    public void downloadEmails() {
+        for (var provider : providers) {
+            provider.downloadEmails();
+        }
+    }
+}
