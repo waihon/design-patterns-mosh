88df1eb22c8a419575d5a4fb4cf7638e8d741afe
Waihon Yew <yewwaihon@gmail.com>
2025-11-07 23:01:34 +0800

    Create an Artefact interface

--- src/main/java/com/waihon/designpatterns/mosh/decorator/exercise/Artefact.java
+++ src/main/java/com/waihon/designpatterns/mosh/decorator/exercise/Artefact.java
@@ -1,26 +1,5 @@
 package com.waihon.designpatterns.mosh.decorator.exercise;
 
-public class Artefact {
-    private String name;
-    private boolean hasError;
-    private boolean main;
-
-    public Artefact(String name) {
-        this.name = name;
-    }
-
-    public String render() {
-        String errorIcon = hasError ? " [Error]" : "";
-        String mainIcon = main ? " [Main]" : "";
-
-        return String.format("%s%s%s", name, errorIcon, mainIcon);
-    }
-
-    public void setHasError(boolean hasError) {
-        this.hasError = hasError;
-    }
-
-    public void setMain(boolean main) {
-        this.main = main;
-    }
+public interface Artefact {
+    String render();
 }
