5ce89c37e52fe10c9f9891ae142704e3fd661971
Waihon Yew <yewwaihon@gmail.com>
2025-11-07 21:46:17 +0800

    Create Artefact and Editor

---
+++ src/main/java/com/waihon/designpatterns/mosh/decorator/exercise/Editor.java
@@ -0,0 +1,19 @@
+package com.waihon.designpatterns.mosh.decorator.exercise;
+
+public class Editor {
+    public void openProject(String path) {
+        Artefact[] artefacts = {
+                new Artefact("Main"),
+                new Artefact("Demo"),
+                new Artefact("EmailClient"),
+                new Artefact("EmailProvider"),
+        };
+
+        artefacts[0].setMain(true);
+        artefacts[2].setHasError(true);
+
+        for (var artefact : artefacts) {
+            System.out.println(artefact.render());
+        }
+    }
+}
