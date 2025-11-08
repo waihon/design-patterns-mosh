36337a5e002d2bde50a816dc2854a159beb7f068
Waihon Yew <yewwaihon@gmail.com>
2025-11-09 00:06:34 +0800

    Create PointIconFactory which creates and/or returns icon

---
+++ src/main/java/com/waihon/designpatterns/mosh/flyweight/demo/PointIconFactory.java
@@ -0,0 +1,18 @@
+package com.waihon.designpatterns.mosh.flyweight.demo;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PointIconFactory {
+    private Map<PointType, PointIcon> icons = new HashMap<>();
+
+    public PointIcon getPointIcon(PointType type) {
+        if (!icons.containsKey(type)) {
+            var icon = new PointIcon(type, null);
+            icons.put(type, icon);
+        }
+
+        return icons.get(type);
+    }
+}
