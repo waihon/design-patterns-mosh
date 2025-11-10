803a90d7b9e980ec7c12def990899c33d1a1e1dc
Waihon Yew <yewwaihon@gmail.com>
2025-11-10 22:22:34 +0800

    Create Library consisting of ebooks

---
+++ src/main/java/com/waihon/designpatterns/mosh/proxy/demo/Library.java
@@ -0,0 +1,24 @@
+package com.waihon.designpatterns.mosh.proxy.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Library {
+    private Map<String, EbookProxy> ebooks = new HashMap<>();
+
+    public void add(EbookProxy ebook) {
+        var fileName = ebook.getFileName();
+
+        ebooks.put(fileName, ebook);
+    }
+
+    public void openEbook(String fileName) {
+        if (!ebooks.containsKey(fileName)) {
+            System.out.printf("Cannot find ebook '%s'\n.", fileName);
+            return;
+        }
+
+        var ebook = ebooks.get(fileName);
+        ebook.show();
+    }
+}
