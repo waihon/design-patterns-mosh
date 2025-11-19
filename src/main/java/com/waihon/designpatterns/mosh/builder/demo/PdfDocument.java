ef6ac2f5172cb656e26e451117c7cfe8b8dd03fe
Waihon Yew <yewwaihon@gmail.com>
2025-11-19 14:06:28 +0800

    Add PdfDocument which allows adding page

---
+++ src/main/java/com/waihon/designpatterns/mosh/builder/demo/PdfDocument.java
@@ -0,0 +1,7 @@
+package com.waihon.designpatterns.mosh.builder.demo;
+
+public class PdfDocument {
+    public void addPage(String text) {
+        System.out.printf("Adding a page to PDF: %s\n", text);
+    }
+}
