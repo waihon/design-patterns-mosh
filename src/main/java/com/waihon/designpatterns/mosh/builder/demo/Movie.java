eee405c7b896f90422ebd6497a06dca9a57b25b9
Waihon Yew <yewwaihon@gmail.com>
2025-11-19 16:38:36 +0800

    Modify Movie and PdfDocument to return frames and pages respectively

--- src/main/java/com/waihon/designpatterns/mosh/builder/demo/Movie.java
+++ src/main/java/com/waihon/designpatterns/mosh/builder/demo/Movie.java
@@ -1,7 +1,14 @@
 package com.waihon.designpatterns.mosh.builder.demo;
 
 public class Movie {
+    private int frames = 0;
+
     public void addFrame(String text, int duration) {
+        frames++;
         System.out.printf("Adding a frame to the movie: %s [%d]\n", text, duration);
+    }
+
+    public int getFrames() {
+        return frames;
     }
 }
--- src/main/java/com/waihon/designpatterns/mosh/builder/demo/PdfDocument.java
+++ src/main/java/com/waihon/designpatterns/mosh/builder/demo/PdfDocument.java
@@ -1,7 +1,14 @@
 package com.waihon.designpatterns.mosh.builder.demo;
 
 public class PdfDocument {
+    private int pages = 0;
+
     public void addPage(String text) {
+        pages++;
         System.out.printf("Adding a page to PDF: %s\n", text);
+    }
+
+    public int getPages() {
+        return pages;
     }
 }
