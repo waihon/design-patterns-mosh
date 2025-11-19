8112988655cd68a3083492dbb994cdfa9e2eddb4
Waihon Yew <yewwaihon@gmail.com>
2025-11-19 14:02:09 +0800

    Add Slide containing text

---
+++ src/main/java/com/waihon/designpatterns/mosh/builder/demo/Slide.java
@@ -0,0 +1,13 @@
+package com.waihon.designpatterns.mosh.builder.demo;
+
+public class Slide {
+    private String text;
+
+    public Slide(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
