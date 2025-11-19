e53aa70a12e616ae566940da583a96d45566145f
Waihon Yew <yewwaihon@gmail.com>
2025-11-19 21:59:40 +0800

    Test first the behaviours of Document Export

---
+++ src/test/java/com/waihon/designpatterns/mosh/builder/exercise/BuilderTest.java
@@ -0,0 +1,36 @@
+package com.waihon.designpatterns.mosh.builder.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BuilderTest {
+    private Document document;
+
+    @BeforeEach
+    void setUp() {
+        document = new Document();
+        document.add(new Text("Hello, World!"));
+        document.add(new Image("pic1.jpg"));
+    }
+
+    @Test
+    void exportToHtmlFormat() throws Exception {
+        var text = tapSystemOut(() -> {
+            document.export(ExportFormat.HTML, "export.html");
+        });
+
+        assertThat(text.lines()).containsExactly("<html><p>Hello, World!</p><img src=\"pic1.jpg\" /></html>");
+    }
+
+    @Test
+    void exportToTextFormat() throws Exception {
+        var text = tapSystemOut(() -> {
+            document.export(ExportFormat.TEXT, "export.txt");
+        });
+
+        assertThat(text.lines()).containsExactly("Hello, World!");
+    }
+}
